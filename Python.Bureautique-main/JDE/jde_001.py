3d91dd056395b2b079ce7254192fba63df7613d6
caa-jdelaux <159487366+caa-jdelaux@users.noreply.github.com>
2024-05-27 10:05:23 +0100

    change

--- Python.Bureautique-main/JDE/jde_001.py
+++ Python.Bureautique-main/JDE/jde_001.py
@@ -1,3 +1,5 @@
+import math
+
 """
 This script demonstrates basic variable initialization, manipulation, and boolean operations.
 
@@ -60,7 +62,7 @@ print("opérateur inf ou égale:",4<=3)
 print()
 print("# récupération de valeurs")
 # récupération de valeurs
-print("#username = input('Enter your name: '")
+print("#username = input('Enter your name: ')")
 print("#print('Username:', username")
 
 
@@ -71,3 +73,28 @@ varInt = 1
 print("varInt:", varInt, " | Type of varInt:", type(varInt))
 varInt = "Test"
 print("varInt:", varInt, " | Type of varInt:", type(varInt))
+
+
+print("\n# Cast de valeur : conversion type variable")
+# Cast de valeur : conversion type variable
+var1 = "123.4"
+var2 = int(float(var1))
+#f : formatage d'une chaine de caractère avec des variables {}
+print(f"la variable var1 est {var1} | est de type {type(var1)}")
+print("var2: " + str(var2) + " | Type of var2: " + str(type(var2)))
+
+
+pi = math.pi
+# Affichage de la valeur de PI avec 2 chiffres après la virgule
+print("Valeur de PI: {0:0.2f}".format(pi))
+print(f"Valeur de PI: {pi:0.2f}")
+print("Valeur de PI:", round(pi, 2))
+print(f"Valeur de PI: {pi:5.2f} (avec un affiche de 5 carac)")
+print("Valeur de PI:", format(pi, '0>5.2f'))
+print(f"Valeur de PI: {pi:0>5.2f} (avec un affiche de 5 carac + un zéro avant le chiffre)")
+# affiche le raccourci CRTL+K et CTRL+C pour commenter le code sélectionné
+
+print("\n# Exo 1")
+nom = input("Entrez votre nom: ")
+prenom = input("Entrez votre prénom: ")
+print(f"Bonjour {nom.upper()} {prenom.capitalize()} !")
