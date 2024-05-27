1dd731bbf1b2c2991995ebb4ad712775297570ee
caa-jdelaux <159487366+caa-jdelaux@users.noreply.github.com>
2024-05-27 08:19:43 +0100

    Update jde_001.py

--- Python.Bureautique-main/JDE/jde_001.py
+++ Python.Bureautique-main/JDE/jde_001.py
@@ -8,9 +8,24 @@ var_string="Hello"
 var_bool = True
 var_vide = None
 
+print()
+print("#Affichage des variables")
 #Affichage des variables
 print("var_int:",var_int)
 print("var_float:",var_float)
 print("var_string:",var_string)
 print("var_bool:",var_bool)
 print("var_vide:",var_vide)
+
+print()
+print("#Manipulation des données")
+#Manipulation des données
+print("var_int:",var_int+1)
+
+
+print()
+print("#Opérateurs booléens")
+#Opérateurs booléens
+print("Opé ET:",True and True)
+print("Opé OU:",True or False)
+print("Opé NEGATION:",not True)
