79b741e003e2d9afbc0399c5939227f057199965
caa-jdelaux <159487366+caa-jdelaux@users.noreply.github.com>
2024-05-27 10:45:59 +0100

    Update jde_001.py

--- Python.Bureautique-main/JDE/jde_001.py
+++ Python.Bureautique-main/JDE/jde_001.py
@@ -1,4 +1,7 @@
 import math
+import getpass
+import random
+
 
 """
 This script demonstrates basic variable initialization, manipulation, and boolean operations.
@@ -92,9 +95,54 @@ print("Valeur de PI:", round(pi, 2))
 print(f"Valeur de PI: {pi:5.2f} (avec un affiche de 5 carac)")
 print("Valeur de PI:", format(pi, '0>5.2f'))
 print(f"Valeur de PI: {pi:0>5.2f} (avec un affiche de 5 carac + un zéro avant le chiffre)")
-# affiche le raccourci CRTL+K et CTRL+C pour commenter le code sélectionné
+
+print("\n# affiche le raccourci CRTL+K et CTRL+C pour commenter le code sélectionné")
 
 print("\n# Exo 1")
-nom = input("Entrez votre nom: ")
-prenom = input("Entrez votre prénom: ")
-print(f"Bonjour {nom.upper()} {prenom.capitalize()} !")
+# nom = input("Entrez votre nom: ")
+# prenom = input("Entrez votre prénom: ")
+# print(f"Bonjour {nom.upper()} {prenom.capitalize()} !")
+
+# Récupération du nom d'utilisateur de l'ordinateur
+username = getpass.getuser()
+# Affichage du nom d'utilisateur en majuscules
+print(f"Nom d'utilisateur: {username.upper()} ou {username.capitalize()}")
+print(f"Nom d'utilisateur: {str.upper(username)} ou {str.capitalize(username)}")
+
+# Génération de deux nombres aléatoires
+# count = 1
+# while count < 4:
+#     num1 = input("Entrez un nombre: ")
+#     if num1.isdigit():
+#         break
+#     else:
+#         print(f"Erreur: num1 n'est pas un nombre valide. Veuillez réessayer (tentative:{count}).")
+#     count += 1
+#     if count == 4:
+#         print("Erreur: num1 n'est pas un nombre valide. Fin de traitement.")
+#         exit()
+    
+# Conversion de num1 en float
+#num1 = float(num1)
+num1 = random.uniform(1,100)
+num2 = random.uniform(1,100)
+
+# Calcul de la somme des deux nombres
+sum = num1 + num2
+
+# Affichage de la somme
+print("La somme de", num1, "et", num2, "est égale à", format(sum, '0>6.2f'))
+
+print("\n# Exo 2")
+# Saisie du rayon et de la hauteur
+# r = float(input("Entrez le rayon du cône : "))
+# h = float(input("Entrez la hauteur du cône : "))
+r = random.uniform(1,100)
+h = random.uniform(1,100)
+
+# Calcul du volume du cône
+B=(math.pi * r**2)
+V = (1/3) * B * h
+
+# Affichage du volume du cône
+print(f"Le volume du cône est : {format(V, '0.2f')}")
